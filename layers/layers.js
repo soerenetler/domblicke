147182fb4855d05323569f47cf78f4b1bc2025f5
Sören Etler <moin@soerenetler.de>
2025-05-01 11:59:03 +0200

    use online files

--- layers/layers.js
+++ layers/layers.js
@@ -13,16 +13,16 @@ var wms_layers = [];
             })
         });
 
-        var lyr_XYZLayer_1 = new ol.layer.Tile({
-            'title': 'XYZ-Layer',
+        var lyr_DomblickeOnline_1 = new ol.layer.Tile({
+            'title': 'Domblicke Online',
             'opacity': 1.000000,
             
             
             source: new ol.source.XYZ({
             attributions: ' ',
-                url: 'file:///users/sorenetler/Documents/Daten_Projekte/Ansichten_Magdeburger_Dom/Kacheln/{z}/{x}/{y}.png'
+                url: 'https://soerenetler.github.io/domblicke/images/Domblick_Kacheln/{z}/{x}/{y}.png'
             })
         });
 
-lyr_OpenStreetMap_0.setVisible(true);lyr_XYZLayer_1.setVisible(true);
-var layersList = [lyr_OpenStreetMap_0,lyr_XYZLayer_1];
+lyr_OpenStreetMap_0.setVisible(true);lyr_DomblickeOnline_1.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_DomblickeOnline_1];
